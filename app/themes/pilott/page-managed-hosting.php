e72205348e86865e7b08754710103ba24c864ff3
root <root@ip-172-30-0-240.ec2.internal>
2016-11-30 17:17:02 -0500

    add new template for cf7

--- app/themes/pilott/page-managed-hosting.php
+++ app/themes/pilott/page-managed-hosting.php
@@ -7,3 +7,4 @@ get_template_part('templates/global/features');
 get_template_part('templates/solutions/managed-hosting');
 get_template_part('templates/global/copilott-callout');
 get_template_part('templates/global/get-started');
+get_template_part('templates/solutions/managed-hosting-form');
---
+++ app/themes/pilott/templates/solutions/managed-hosting-form.php
@@ -0,0 +1,2 @@
+<?php
+echo do_shortcode( '[contact-form-7 id="48" title="Contact form 1"]' );
