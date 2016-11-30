55635657d6b1ad9b8f6d898238ed9c15ea64dff3
hideokamoto <hide.okamoto@digitalcube.jp>
2016-11-30 14:21:09 -0800

    add markup

--- app/themes/pilott/templates/solutions/managed-hosting-form.php
+++ app/themes/pilott/templates/solutions/managed-hosting-form.php
@@ -1,2 +1,12 @@
-<?php
-echo do_shortcode( '[contact-form-7 id="48" title="Contact form 1"]' );
+<section class="section-padding _green">
+  <article class="container">
+    <header class="section-lead white">
+      <h1 class="lg-sans">Contact us for advice, support,<br>or additional information.</h1>
+    </header>
+    <section class="contact">
+      <?php
+        echo do_shortcode( '[contact-form-7 id="48" title="Contact form 1"]' );
+      ?>
+    </section>
+  </article>
+</section>
