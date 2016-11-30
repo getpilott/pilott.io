baaa324946fd1e1d9e7c779ebe10672739cea703
hideokamoto <hide.okamoto@digitalcube.jp>
2016-11-30 14:22:57 -0800

    update form template

--- app/themes/pilott/page-woocommerce-hosting.php
+++ app/themes/pilott/page-woocommerce-hosting.php
@@ -8,4 +8,5 @@ get_template_part('templates/solutions/woocommerce-hosting');
 get_template_part('templates/solutions/woocommerce-key-benefits');
 get_template_part('templates/solutions/addons');
 get_template_part('templates/global/copilott-callout');
-get_template_part('templates/global/get-started');
+#get_template_part('templates/global/get-started');
+get_template_part('templates/solutions/woocommerce-hosting-form');
