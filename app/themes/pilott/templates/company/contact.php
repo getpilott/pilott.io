0c17b40198d4390ef84f83789a53be71537d87aa
hideokamoto <hide.okamoto@digitalcube.jp>
2016-11-30 22:17:07 -0800

    change form as cf7

--- app/themes/pilott/templates/company/contact.php
+++ app/themes/pilott/templates/company/contact.php
@@ -4,36 +4,13 @@
  */
 ?>
 
-<section class="section-padding _green">
+<section class="section-padding _green" id="contact">
   <article class="container">
     <header class="section-lead white">
       <h1 class="lg-sans">Contact us for advice, support,<br />or additional information.</h1>
     </header>
     <section class="contact">
-      <form class="contact__form" action="" method="post">
-        <div>
-          <input id="name" type="text" name="name" value="">
-          <label class="white xs-sans sm-caps" for="name">Name</label>
-        </div>
-        <div>
-          <input id="company" type="text" name="company" value="">
-          <label class="white xs-sans sm-caps" for="company">Company</label>
-        </div>
-        <div>
-          <input id="email" type="email" name="email" value="">
-          <label class="white xs-sans sm-caps" for="email">Email</label>
-        </div>
-        <div>
-          <input id="url" type="url" name="url" value="">
-          <label class="white xs-sans sm-caps" for="url">URL</label>
-        </div>
-        <div class="contact__message">
-          <textarea name="message" rows="8" placeholder="MESSAGE:"></textarea>
-        </div>
-        <div class="contact__submit">
-          <a href="#" class="btn-outline--white">Submit</a>
-        </div>
-      </form>
+      <?php echo do_shortcode('[contact-form-7 id="50" title="Company contact"]');?>
     </section>
   </article>
 </section>
