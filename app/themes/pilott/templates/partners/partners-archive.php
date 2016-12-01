2e89c25c83069013689b5f89e9c500f2af7ae3d1
Daniel Olson <emaildano@gmail.com>
2016-12-01 03:14:04 -0800

    updated partners page

---
+++ app/themes/pilott/templates/partners/partners-archive.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Partners: Archive
+ */
+?>
+
+<section class="_gray-light section-padding">
+  <div class="container partners__list">
+    <article class="partners__item _white match-h">
+      <div class="partners__icon">
+        <img class="img-g" width="250" height="auto" src="<?= get_template_directory_uri();?>/dist/images/aws_partnernetwork.png" alt="">
+      </div>
+      <h2 class="partners__title sm-sans black-light">AWS Advanced Consulting Partner</h2>
+      <div class="partners__desc">
+        <p>A trusted partner in the Amazon Web Services (AWS) partner network.</p>
+      </div>
+      <a class="partners__link btn-link" href="https://aws.amazon.com/partners/">Learn More</a>
+    </article>
+    <article class="partners__item _white match-h">
+      <div class="partners__icon">
+        <img class="img-g" width="250" height="auto" src="<?= get_template_directory_uri();?>/dist/images/rublon.png" alt="">
+      </div>
+      <h2 class="partners__title sm-sans black-light">Rublon</h2>
+      <div class="partners__desc">
+        <p>Rublon Account Security features two-factor authentication for web-based businesses.</p>
+      </div>
+      <a class="partners__link btn-link" href="https://rublon.com/">Learn More</a>
+    </article>
+    <article class="partners__item _white match-h">
+      <div class="partners__icon">
+        <img class="img-g" width="250" height="auto" src="<?= get_template_directory_uri();?>/dist/images/siterock.png" alt="">
+      </div>
+      <h2 class="partners__title sm-sans black-light">Site Rock</h2>
+      <div class="partners__desc">
+        <p>Site Rock Co., Ltd. is a management service provider that offers 24/7/365 high quality operational monitoring services through human resources, process, and systems via the internet (MSP).</p>
+      </div>
+      <a class="partners__link btn-link" href="https://www.siterock.co.jp/">Learn More</a>
+    </article>
+    <article class="partners__item _white match-h">
+      <div class="partners__icon">
+        <img class="img-g" width="250" height="auto" src="<?= get_template_directory_uri();?>/dist/images/cloudpack_logo.png" alt="">
+      </div>
+      <h2 class="partners__title sm-sans black-light">cloudpack</h2>
+      <div class="partners__desc">
+        <p>cloudpack is a AWS top specialist that introduces AWS architecture, environment, and operation—including services from maintenance to total managed hosting service support.</p>
+      </div>
+      <a class="partners__link btn-link" href="https://cloudpack.jp/en/">Learn More</a>
+    </article>
+    <article class="partners__item _white match-h">
+      <div class="partners__icon">
+        <img class="img-g" width="250" height="auto" src="<?= get_template_directory_uri();?>/dist/images/j2-logo--black-outlne.svg" alt="">
+      </div>
+      <h2 class="partners__title sm-sans black-light">J2 Design</h2>
+      <div class="partners__desc">
+        <p>Based in Philadelphia, J2 Design is a team of designers, programmers, and strategists who are passionate about evolving their clients’ stories and delivering transformative design solutions.</p>
+      </div>
+      <a class="partners__link btn-link" href="https://j2made.com">Learn More</a>
+    </article>
+  </div>
+</section>
